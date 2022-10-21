d09e0edf648fa9a86e013107b71941148fda76a9
SNHaritha <116284716+SNHaritha@users.noreply.github.com>
2022-10-21 15:11:55 +0530

    Create autosar_com.c

---
+++ autosar_com.c
@@ -0,0 +1,16 @@
+
+
+
+#include <xc.h>
+
+
+#include "canif.h"
+#include "autosar_communicatconfig.h"
+
+void CAN_Send_signal(int CAN_SIG,int Value)
+{    
+
+//    AUTOSAR_MSG1_S.sig_1_s = Value;       
+//     CAN_FRAME(0,data);
+
+}
